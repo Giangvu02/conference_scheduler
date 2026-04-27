89de57c5c15de9d823b3f1b6d9dcbe58de7ab976
Giang-02 <giangvuko@giangvukogmail.gvukogmailcom>
2026-04-27 14:39:35 +0700

    Fix SpeakerType namespace, update Speaker edit view and User detail layout

--- src/Form/SpeakerType.php
+++ src/Form/SpeakerType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Speaker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,11 +15,21 @@ class SpeakerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('topic', TextType::class)
-            ->add('biography', TextareaType::class)
-            ->add('photo', TextType::class);
+            ->add('name', TextType::class, [
+                'label' => 'Họ và tên',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('topic', TextType::class, [
+                'label' => 'Chủ đề diễn thuyết',
+            ])
+            ->add('biography', TextareaType::class, [
+                'label' => 'Tiểu sử',
+            ])
+            ->add('photo', TextType::class, [
+                'label' => 'Đường dẫn ảnh (URL)',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
